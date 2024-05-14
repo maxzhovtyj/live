d2edf2c7ebede103522985b1c504470e77ed9bd3
Zhovtaniuk <zhovtyjshady@gmail.com>
2024-05-14 09:36:26 +0300

    sqlc

--- db/queries/queries.sql
+++ db/queries/queries.sql
@@ -1,10 +1,5 @@
--- name: GetUser :one
-SELECT *
-FROM users
-WHERE id = ?
-LIMIT 1;
-
 -- name: CreateUser :one
-INSERT INTO users (first_name, last_name, email, password_hash)
-VALUES (?, ?, ?, ?)
-RETURNING *;
+INSERT INTO users (first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4) RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users WHERE id = $1 LIMIT 1;
