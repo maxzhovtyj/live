282e1fa84b2cc550e5d047f4d17069919b889805
Zhovtaniuk <zhovtyjshady@gmail.com>
2024-05-26 12:06:48 +0300

    styles

--- internal/pkg/templates/video/meeting_templ.go
+++ internal/pkg/templates/video/meeting_templ.go
@@ -34,7 +34,7 @@ func MeetingPage(c models.Context) templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8\"><form hx-ext=\"response-targets\" hx-post=\"/create-meeting\" hx-target-*=\"#serious-errors\" class=\"flex justify-center space-y-6\"><button type=\"submit\" class=\"group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500\">Create Meeing</button></form></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"container my-24 mx-auto md:px-6\"><section class=\"mb-32 text-center\"><div class=\"flex justify-center\"><div class=\"max-w-[800px]\"><h2 class=\"mb-12 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl\">Are you ready <br><span class=\"text-primary dark:text-primary-400\">For the meeting?</span></h2><p class=\"text-lg text-neutral-500 dark:text-neutral-300\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia consequatur adipisci tenetur repudiandae rerum quos.</p><form hx-ext=\"response-targets\" hx-post=\"/create-meeting\" hx-target-*=\"#serious-errors\" class=\"flex justify-center space-y-6 mt-4\"><button type=\"submit\" class=\"btn btn-primary\">Create Meeing</button></form></div></div></section></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
